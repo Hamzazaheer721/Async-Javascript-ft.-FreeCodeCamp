2ef98255accd6ab2f86f96bec93640633a4b93d5
Hamza Zaheer <hamzazaheer721@gmail.com>
2021-11-29 15:21:45 +0500

    !feat: callback hell has been created

--- 3_iceCream.ts
+++ 3_iceCream.ts
@@ -19,6 +19,18 @@ const production = (): void => {
       console.log("Fruits have been chopped")
       setTimeout(()=>{
         console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} was added`)
+        setTimeout(()=>{
+          console.log("The machine was started")
+          setTimeout(()=>{
+            console.log(`${stocks.holder[0]} was selected`)
+            setTimeout(()=>{
+              console.log(`${stocks.toppings[0]} has been added as toppings`)
+              setTimeout(()=>{
+                console.log("Ice cream has been served")
+              },1000)
+            },2000)
+          },2000)
+        },1000)
       },1000)
     },2000)
   },1000)
